ec4e188b069a1ef9f0d0f319177b8c34773a7098
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-09-29 01:29:32 -0300

    Proyecto Barbershop. Header

---
+++ main.js
@@ -0,0 +1,16 @@
+let btnMenu = document.getElementById("btnMenu");
+let navbar = document.getElementById("navbar");
+let menuNav = document.getElementById("menuNav");
+
+navbar.style.right = "-250px";
+
+btnMenu.onclick = function() {
+    if(navbar.style.right == "-250px"){
+        navbar.style.right = "0";
+        menuNav.src = "img/menuNav2.png"
+    }
+    else{
+        navbar.style.right = "-250px";
+        menuNav.src = "img/menuNav.png"
+    }
+}
