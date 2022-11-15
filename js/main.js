1aa37d287fff596047e836f11c4642b919c4d408
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-11-14 21:22:42 -0300

    scrollReveal

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ let scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000,
 	speedAsDuration: true
 });
+
+// SCROLL REVEAL EFECT
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 250,
+});
+
+    sr.reveal(`.tittleText`, {delay:100})
+    sr.reveal(`.cardEstilos`, {origin:'left', delay:150})
+    sr.reveal(`.imgEstilos`, {origin:'right', delay:150})
+    sr.reveal(`.boxServicios`, {delay:150})
+    sr.reveal(`.boxEquipo`, {origin:'bottom', delay:150})
+    sr.reveal(`.imgEquipo`, {origin:'left', delay:150})
+    sr.reveal(`.imgEquipo2`, {origin:'right', delay:150})
+    sr.reveal(`.formBox`, {origin:'bottom', delay:200})
+    sr.reveal(`.footerLeft`, {origin:'left', delay:50})
+    sr.reveal(`.footerRight`, {origin:'right', delay:50})
